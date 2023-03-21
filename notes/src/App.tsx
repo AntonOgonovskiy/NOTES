60597e236a08ac28ffae3f1b863c628dce9dcd86
Anton <antonogonovskiy@gmail.com>
2023-03-21 17:18:53 +0300

    remove tags field

--- notes/src/App.tsx
+++ notes/src/App.tsx
@@ -3,14 +3,12 @@ import "./App.scss";
 import Header from "./components/header/header";
 import AskNameModal from "./components/nameModal/AskNameModal";
 import NotesField from "./components/body/notesField/notesField";
-import TagsField from "./components/body/tagsField/tagsField";
 
 function App() {
   return (
     <div className="App">
       <AskNameModal />
       <Header />
-      <TagsField />
       <NotesField />
     </div>
   );
